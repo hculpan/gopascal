c45698ff1941a7a8e972f0ba07d5185a1da6fb02
Harry Culpan <harry@culpan.org>
2020-11-20 15:53:57 -0500

    Just a test program

--- main.go
+++ main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
-	fmt.Println("This is my program")
+	var x []int = []int{10, 5, 4, -10, 1000}
+	var y []int = []int{0, 0}
+
+	copy(y, x[3:5])
+	z := append(y, -999)
+
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
 }
